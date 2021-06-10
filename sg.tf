ee8e40621959c48f7948a16a2062c62cc946be20
Michal Plebanski <michalp@airnauts.com>
2021-06-10 10:15:26 +0200

    Add port 80 to lambda egress SG

--- sg.tf
+++ sg.tf
@@ -17,6 +17,13 @@ resource "aws_security_group" "lambda" {
   }
 
   egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
     from_port   = 53
     to_port     = 53
     protocol    = "tcp"
